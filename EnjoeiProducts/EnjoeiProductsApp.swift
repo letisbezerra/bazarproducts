a2161d358b0af18ff3d9368b4ec993ebae91b542
Leticia Bezerra <leticiabezerrauy@gmail.com>
2026-07-01 19:31:30 -0300

    Initial Commit

---
+++ EnjoeiProducts/EnjoeiProductsApp.swift
@@ -0,0 +1,17 @@
+//
+//  EnjoeiProductsApp.swift
+//  EnjoeiProducts
+//
+//  Created by Leticia Bezerra on 01/07/26.
+//
+
+import SwiftUI
+
+@main
+struct EnjoeiProductsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
